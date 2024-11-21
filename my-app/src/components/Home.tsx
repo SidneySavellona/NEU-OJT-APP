3505508f72483d423374165497380b6d561361c1
Sidney Savellona <153743474+SidneySavellona@users.noreply.github.com>
2024-11-21 22:52:28 +0800

    Create Home.tsx

---
+++ my-app/src/components/Home.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "../styles/App.css";
+
+const Home: React.FC = () => {
+    return (
+        <button className="home-button">
+            <img src="neu_logo.png" alt="Home" />
+        </button>
+    );
+};
+
+export default Home;
