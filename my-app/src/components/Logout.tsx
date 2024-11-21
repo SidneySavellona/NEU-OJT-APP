513f5d991013c19ac5b7d5781a15d98bdce558ed
Sidney Savellona <153743474+SidneySavellona@users.noreply.github.com>
2024-11-21 22:51:24 +0800

    Create Logout.tsx

---
+++ my-app/src/components/Logout.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "../styles/App.css";
+
+const Logout: React.FC = () => {
+    return (
+        <button className="logout-button">
+            Logout
+        </button>
+    );
+};
+
+export default Logout;
