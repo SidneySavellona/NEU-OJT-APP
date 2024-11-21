c938ac1edf1684f7b3d47b9c88fcce7b32b2c23e
Sidney Savellona <153743474+SidneySavellona@users.noreply.github.com>
2024-11-21 22:52:02 +0800

    Create Name.tsx

---
+++ my-app/src/components/Name.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "../styles/App.css"
+
+const Name: React.FC = () => {
+    return (
+        <h1 className = 'name'>NEU OJT APP</h1>
+    )
+}
+
+export default Name;
