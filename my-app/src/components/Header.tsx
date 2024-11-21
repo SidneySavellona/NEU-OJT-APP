80eaf2e6a5aae1b81b8ddf1a96287fa3a7875ed1
Sidney Savellona <153743474+SidneySavellona@users.noreply.github.com>
2024-11-21 22:50:53 +0800

    Create Header.tsx

---
+++ my-app/src/components/Header.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Home from "./Home";
+import Name from "./Name";
+import Logout from "./Logout";
+import "../styles/App.css"
+
+const Header: React.FC = () => {
+    return (
+           <div className = 'header'>
+                <Home />
+                <Name />
+                <Logout />
+           </div>
+    )
+}
+
+export default Header;
