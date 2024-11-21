bfb188cc23a0a67b70e78cc2f7ce12e605c41519
Sidney Savellona <153743474+SidneySavellona@users.noreply.github.com>
2024-11-21 22:52:54 +0800

    Create MainOptions.tsx

---
+++ my-app/src/components/MainOptions.tsx
@@ -0,0 +1,17 @@
+
+import React from "react";
+import "../styles/App.css";
+
+const MainOptions: React.FC = () => {
+
+    return(
+        <section className="options">
+            <button className="option-button"><img src="neu_logo.png" alt="Enter Student Input" />Enter Student Input</button>
+            <button className="option-button"><img src="neu_logo.png" alt="Upload Requirements" />Upload Requirements</button>
+            <button className="option-button"><img src="neu_logo.png" alt="Generate Endorsement Letter" />Generate Endorsement Letter</button>
+            <button className="option-button"><img src="neu_logo.png" alt="Update Company" />Update Company</button>
+        </section>
+    )
+}
+
+export default MainOptions;
