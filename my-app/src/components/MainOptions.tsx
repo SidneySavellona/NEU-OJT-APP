eb5119c9856e87b23d8fc292d1271b1738077d2b
jdy <dyiiijaime@gmail.com>
2024-12-02 19:21:01 +0800

    changed cursor for option buttons

--- my-app/src/components/MainOptions.tsx
+++ my-app/src/components/MainOptions.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useNavigate } from "react-router-dom"; 
-import styles from "../styles/Dashboard.module.css"; 
+import "../styles/Dashboard.css"
 
 const MainOptions: React.FC = () => {
   const navigate = useNavigate(); // Initialize useNavigate
